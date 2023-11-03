d96c9927a7531b0bf2b77cbaa43c1f4d62e9693f
AnggiAzizah1 <anggi.ajijah@gmail.com>
2023-11-03 23:52:55 +0700

    commentt

--- app/src/test/java/com/me/calculator/CalculatorTest.java
+++ app/src/test/java/com/me/calculator/CalculatorTest.java
@@ -10,3 +10,6 @@ public class CalculatorTest {
         int result = calculator.add(10, 10);
     }
 }
+//Buat kelas uji JUnit untuk menguji kelas "Calculator". Nama kelas uji harus mengikuti konvensi dengan akhiran "Test".
+//Kelas di atas mendefinisikan satu metode pengujian "testAddSucces"
+// setlah itu uji project yang telah dibuat.
