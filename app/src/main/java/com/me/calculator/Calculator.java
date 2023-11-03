18973a3aa5ce5046414d1fc020f211a23ddf2db9
AnggiAzizah1 <anggi.ajijah@gmail.com>
2023-11-03 23:49:51 +0700

    comment

--- app/src/main/java/com/me/calculator/Calculator.java
+++ app/src/main/java/com/me/calculator/Calculator.java
@@ -1,5 +1,6 @@
 package com.me.calculator;
-
+//Buat proyek Java dan tambahkan JUnit ke proyek yang dibuat.
+// Pastikan telah mengunduh JUnit dan menambahkannya ke Build Path proyek Anda.
 public class Calculator {
     public Integer add(Integer a, Integer b){
         return a+b;
